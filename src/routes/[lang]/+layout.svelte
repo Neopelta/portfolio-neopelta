a9ffb89e3f9a735a21ee44e1867ae57ff0786f8e
Neopelta <ronan.pluta@gmail.com>
2025-07-31 19:38:15 +0200

    chore: delete consolelog

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { setContext } from 'svelte';
-	import { afterNavigate } from '$app/navigation';
 	import { writable } from 'svelte/store';
 
 	export let data;
@@ -9,10 +8,6 @@
 	$: langStore.set(data.lang);
 
 	setContext('lang', langStore);
-
-	afterNavigate(({ to }) => {
-		console.log('Navigated to:', to.url.pathname);
-	});
 </script>
 
 <slot />
